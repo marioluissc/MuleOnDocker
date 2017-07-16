6942a02e8da72a91c8e94107613ff502e4d9d683
Mario Luis Scarpari Citadin <mario.citadin@opencolleges.edu.au>
2017-07-16 19:10:15 +1000

    Mule Agent

--- MuleAgent/amc_setup.bat
+++ MuleAgent/amc_setup.bat
@@ -5,5 +5,5 @@ set REALPATH=%~dp0
 set BASE_DIR=%REALPATH:~0,-5%
 
 IF NOT EXIST "%BASE_DIR%\tools" mkdir "%BASE_DIR%\tools"
-IF EXIST "%BASE_DIR%\bin\agent-setup-1.4.1-amc-final.jar" move "%BASE_DIR%\bin\agent-setup-1.4.1-amc-final.jar" "%BASE_DIR%\tools"
-java -jar "%BASE_DIR%\tools\agent-setup-1.4.1-amc-final.jar" %*
+IF EXIST "%BASE_DIR%\bin\agent-setup-1.7.0-mule-3.x-amc-final.jar" move "%BASE_DIR%\bin\agent-setup-1.7.0-mule-3.x-amc-final.jar" "%BASE_DIR%\tools"
+java -jar "%BASE_DIR%\tools\agent-setup-1.7.0-mule-3.x-amc-final.jar" %*
